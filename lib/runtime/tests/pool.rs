997d2c20c07c7e4de573ad2cc781dd46be2bea1f
Ryan Olson <rolson@nvidia.com>
2025-03-03 01:09:33 -0800

    clippy/fmt

--- lib/runtime/tests/pool.rs
+++ lib/runtime/tests/pool.rs
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use async_trait::async_trait;
 use std::sync::{Arc, Mutex};
 use tokio::sync::Notify;
 use triton_distributed_runtime::utils::pool::{
