64b5721f591abea54a65b86b581ca0e650d32a3c
Piotr Marcinkiewicz <piotrm@nvidia.com>
2025-03-23 20:02:32 +0100

    Replce container for other routers

--- lib/bindings/python/rust/lib.rs
+++ lib/bindings/python/rust/lib.rs
@@ -412,7 +412,10 @@ impl Client {
 
         pyo3_async_runtimes::tokio::future_into_py(py, async move {
             let stream = client.random(request.into()).await.map_err(to_pyerr)?;
-            let ignore_response = request.contains_key("ignore_response") && request["ignore_response"].as_bool().unwrap_or(false);
+            let ignore_response = request
+                .get("ignore_response")  // returns an Option<&Value> if request is an object
+                .and_then(|value| value.as_bool())
+                .unwrap_or(false);
             tokio::spawn(process_stream(stream, tx, ignore_response));
             Ok(AsyncResponseStream {
                 rx: Arc::new(Mutex::new(rx)),
@@ -442,7 +445,10 @@ impl Client {
                 .await
                 .map_err(to_pyerr)?;
 
-            let ignore_response = request.contains_key("ignore_response") && request["ignore_response"].as_bool().unwrap_or(false);
+            let ignore_response = request
+                .get("ignore_response")  // returns an Option<&Value> if request is an object
+                .and_then(|value| value.as_bool())
+                .unwrap_or(false);
             tokio::spawn(process_stream(stream, tx, ignore_response));
 
             Ok(AsyncResponseStream {
