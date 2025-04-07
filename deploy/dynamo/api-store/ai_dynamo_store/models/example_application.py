d3f4e45224225cc6607b9841730806840833bd08
Hannah Zhang <hannahz@nvidia.com>
2025-04-07 11:16:23 -0700

    feat: add cursor-gen python script for CRD

---
+++ deploy/dynamo/api-store/ai_dynamo_store/models/example_application.py
@@ -0,0 +1,41 @@
+from ai_dynamo_store.models.dynamo_deployment import DynamoDeployment, DynamoDeploymentSpec, TypeMeta, ObjectMeta
+
+# Create a new deployment
+deployment = DynamoDeployment(
+    type_meta=TypeMeta(
+        api_version="nvidia.com/v1alpha1",
+        kind="DynamoDeployment"
+    ),
+    object_meta=ObjectMeta(
+        name="my-deployment",
+        namespace="my-namespace",
+        labels={
+            "ngc-organization": "my-org",
+            "ngc-user": "my-user"
+        }
+    ),
+    spec=DynamoDeploymentSpec(
+        dynamo_nim="my-nim",
+        services={
+            "service1": {
+                "spec": {
+                    "replicas": 3,
+                    "resources": {
+                        "requests": {
+                            "cpu": "500m",
+                            "memory": "1Gi"
+                        }
+                    }
+                }
+            }
+        }
+    )
+)
+
+# The model will handle camelCase to snake_case conversion automatically
+# You can access fields using snake_case in Python
+print(deployment.object_meta.name)  # "my-deployment"
+print(deployment.type_meta.api_version)  # "nvidia.com/v1alpha1"
+
+# When serializing to JSON, it will use camelCase
+json_data = deployment.model_dump_json(by_alias=True)
