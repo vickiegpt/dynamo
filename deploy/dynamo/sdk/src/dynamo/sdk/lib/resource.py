283a5424a6aec10e56410adb1791e54c15cffb08
ishandhanani <ishandhanani@gmail.com>
2025-04-17 07:52:15 +0000

    me

--- deploy/dynamo/sdk/src/dynamo/sdk/lib/resource.py
+++ deploy/dynamo/sdk/src/dynamo/sdk/lib/resource.py
@@ -1,4 +1,3 @@
-#  SPDX-FileCopyrightText: Copyright (c) 2020 Atalaya Tech. Inc
 #  SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #  SPDX-License-Identifier: Apache-2.0
 #  #
@@ -13,7 +12,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-#  Modifications Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES
 
 # TODO: this should be used for planner as well and should leverage proper nvml bindings
 
