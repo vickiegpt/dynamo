0c4a2daedf21cefa8886c3c098bb573e7bb0d6cc
ishandhanani <ishandhanani@gmail.com>
2025-04-17 06:17:01 +0000

    new

---
+++ deploy/dynamo/sdk/src/dynamo/sdk/lib/exceptions.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from http import HTTPStatus
+
+
+class DynamoException(Exception):
+    """Base class for all Dynamo SDK errors."""
+    error_code = HTTPStatus.INTERNAL_SERVER_ERROR
+    error_mapping = {}
+
+    def __init_subclass__(cls) -> None:
+        if "error_code" in cls.__dict__:
+            cls.error_mapping[cls.error_code] = cls
+
+    def __init__(self, message: str, error_code: HTTPStatus | None = None):
+        super().__init__(message)
+        self.message = message
+        self.error_code = error_code or self.error_code
