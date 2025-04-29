081e286e726769ff74e31c84a476eec35c3aebf3
Ryan Olson <rolson@nvidia.com>
2025-04-29 19:29:52 +0000

    kvmb top-level config in progress

--- lib/llm/src/block_manager.rs
+++ lib/llm/src/block_manager.rs
@@ -25,19 +25,98 @@ pub mod layout;
 pub mod pool;
 pub mod storage;
 
-// pub use block::{Block, BlockMetadata};
-// pub use layout::BlockLayout;
-// pub use pool::BlockPool;
+pub use crate::common::dtype::DType;
+pub use block::{BasicMetadata, BlockMetadata};
+pub use layout::{LayoutConfig, LayoutType};
+pub use pool::BlockPool;
+pub use storage::{
+    nixl::NixlEnabledStorage, DeviceStorage, PinnedStorage, Storage, StorageAllocator,
+};
+pub use tokio_util::sync::CancellationToken;
 
-// pub struct KvBlockManager<
-//     DeviceLayout: BlockLayout,
-//     DeviceMetadata: BlockMetadata,
-//     HostLayout: BlockLayout,
-//     HostMetadata: BlockMetadata,
-//     LocalStorageLayout: BlockLayout,
-//     LocalStorageMetadata: BlockMetadata,
-// > {
-//     device_pool: BlockPool<DeviceLayout, DeviceMetadata>,
-//     host_pool: BlockPool<HostLayout, HostMetadata>,
-//     local_storage_pool: BlockPool<LocalStorageLayout, LocalStorageMetadata>,
-// }
+use derive_builder::Builder;
+use nixl_sys::Agent as NixlAgent;
+use std::sync::Arc;
+use validator::Validate;
+
+pub struct KvBlockManagerBuilder<
+    DeviceMetadata: BlockMetadata,
+    HostMetadata: BlockMetadata,
+    // LocalStorageMetadata: BlockMetadata,
+> {
+    host_pool: Option<BlockPool<PinnedStorage, HostMetadata>>,
+    device_pool: Option<BlockPool<DeviceStorage, DeviceMetadata>>,
+    // local_storage_pool: Option<BlockPool<LocalStorageLayout, LocalStorageMetadata>>,
+    nixl_agent: Option<NixlAgent>,
+}
+
+#[derive(Debug, Clone, Builder)]
+pub struct KvManagerRuntimeConfig {
+    worker_id: u64,
+
+    #[builder(default)]
+    cancellation_token: CancellationToken,
+
+    #[builder(default = "true")]
+    enable_nixl: bool,
+}
+
+#[derive(Debug, Clone, Builder, Validate)]
+pub struct KvManagerModelConfig {
+    #[validate(range(min = 1))]
+    pub num_layers: usize,
+
+    #[validate(range(min = 1))]
+    pub page_size: usize,
+
+    #[validate(range(min = 1))]
+    pub inner_dim: usize,
+
+    #[builder(default = "DType::FP16")]
+    pub dtype: DType,
+}
+
+#[derive(Clone, Builder, Validate)]
+#[builder(build_fn(validate = "Self::validate"))]
+pub struct KvManagerBlockConfig<S: Storage + NixlEnabledStorage> {
+    #[validate(range(min = 1))]
+    pub num_blocks: usize,
+
+    #[builder(default = "LayoutType::FullyContiguous")]
+    pub layout_type: LayoutType,
+
+    /// Storage for the blocks
+    /// If provided, the blocks will be allocated from the provided storage
+    /// Otherwise, the blocks will be allocated from
+    #[builder(default)]
+    pub storage: Option<Vec<S>>,
+
+    /// If provided, the blocks will be allocated from the provided allocator
+    /// This option is mutually exclusive with the `storage` option
+    #[builder(default, setter(custom))]
+    pub allocator: Option<Arc<dyn StorageAllocator<S>>>,
+}
+
+// Implement the validation and build functions on the generated builder type
+// Note: derive_builder generates KvManagerBlockConfigBuilder<S>
+impl<S: Storage> KvManagerBlockConfigBuilder<S> {
+    /// Custom setter for the `allocator` field
+    fn allocator(&mut self, allocator: impl StorageAllocator<S> + 'static) -> &mut Self {
+        self.allocator = Some(Some(Arc::new(allocator)));
+        self
+    }
+
+    // Validation function
+    fn validate(&self) -> Result<(), String> {
+        match (self.storage.is_some(), self.allocator.is_some()) {
+            (true, false) | (false, true) => Ok(()), // XOR condition met
+            (true, true) => Err("Cannot provide both `storage` and `allocator`.".to_string()),
+            (false, false) => Err("Must provide either `storage` or `allocator`.".to_string()),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Builder, Validate)]
+pub struct KvManagerPoolConfig {
+    model_config: KvManagerModelConfig,
+}
