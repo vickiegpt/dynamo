47e8c02e329dfd3aede359bdb3b9b991b3ec8568
oandreeva-nv <oandreeva@nvidia.com>
2025-05-08 18:27:02 -0700

    refactor

--- lib/engines/mistralrs/src/lib.rs
+++ lib/engines/mistralrs/src/lib.rs
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-use std::any;
 use std::collections::HashMap;
 use std::{num::NonZero, sync::Arc};
 
@@ -236,7 +235,6 @@ impl MistralRsEngine {
             return_raw_logits: false,
             web_search_options: None,
         });
-        tracing::debug!(request_id, "Sending warmup request");
 
         // Send warmup request and consume response
         if let Ok(sender) = engine.mistralrs.get_sender() {
