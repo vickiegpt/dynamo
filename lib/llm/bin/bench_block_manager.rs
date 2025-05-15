b91bfea0ed3931eb4a96f17d8ec40ac13009f172
jothomson <jwillthomson19@gmail.com>
2025-05-14 17:29:10 -0700

    Simple block manager benchmarks

---
+++ lib/llm/bin/bench_block_manager.rs
@@ -0,0 +1,496 @@
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use dynamo_llm::block_manager::block::BlockState;
+use dynamo_llm::{
+    block_manager::{
+        block::BlockExt,
+        storage::cuda::{DeviceAllocator, PinnedAllocator},
+        BasicMetadata, KvBlockManager, KvBlockManagerConfig, KvManagerLayoutConfig,
+        KvManagerModelConfig, KvManagerRuntimeConfig,
+    },
+    tokens::{PartialTokenBlock, SequenceHash, Tokens},
+};
+use indicatif::ProgressIterator;
+use ordered_float::NotNan;
+use plotters::prelude::*;
+use rand::{rngs::ThreadRng, Rng};
+use std::future::Future;
+use std::ops::Range;
+use std::sync::Arc;
+use tokio::sync::{mpsc, Mutex};
+use tokio::time::{interval, Duration, Instant};
+
+#[derive(Parser)]
+struct Args {
+    #[clap(short = 'd', long, default_value_t = 512)]
+    num_device_blocks: usize,
+
+    #[clap(short = 'n', long, default_value_t = 512)]
+    num_host_blocks: usize,
+
+    #[clap(long, default_value_t = 24)]
+    num_layers: usize,
+
+    #[clap(long, default_value_t = 4096)]
+    inner_dim: usize,
+
+    #[clap(long, default_value_t = 32)]
+    block_size: usize,
+
+    #[clap(short = 't', long, default_value_t = 60)]
+    duration_s: usize,
+
+    #[clap(short = 'r', long, default_value_t = 10)]
+    requests_per_second: usize,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+trait TokenGenerator: Send + Sync {
+    fn next(&self, rng: &mut ThreadRng) -> Result<Vec<u32>>;
+}
+
+#[derive(Parser, Clone)]
+struct RandomTokenGeneratorArgs {
+    /// Number of tokens to generate
+    #[clap(short, long, default_value_t = 1536)]
+    num_tokens: usize,
+}
+
+struct RandomTokenGenerator {
+    num_tokens: usize,
+}
+
+impl From<RandomTokenGeneratorArgs> for RandomTokenGenerator {
+    fn from(args: RandomTokenGeneratorArgs) -> Self {
+        RandomTokenGenerator {
+            num_tokens: args.num_tokens,
+        }
+    }
+}
+
+impl TokenGenerator for RandomTokenGenerator {
+    fn next(&self, rng: &mut ThreadRng) -> Result<Vec<u32>> {
+        Ok((0..self.num_tokens)
+            .map(|_| rng.random_range(0..1024))
+            .collect())
+    }
+}
+
+#[derive(Parser, Clone)]
+struct HierarchicalTokenGeneratorArgs {
+    /// Amount of groups
+    #[clap(short, long, default_value_t = 3)]
+    num_groups: usize,
+
+    /// Group size
+    #[clap(short, long, default_value_t = 512)]
+    group_size: usize,
+
+    /// Amount of different sequences per group
+    #[clap(short, long, default_value_t = 10)]
+    variations_per_group: usize,
+}
+
+struct HierarchicalTokenGenerator {
+    num_groups: usize,
+    group_size: usize,
+    variations_per_group: usize,
+}
+
+impl From<HierarchicalTokenGeneratorArgs> for HierarchicalTokenGenerator {
+    fn from(args: HierarchicalTokenGeneratorArgs) -> Self {
+        HierarchicalTokenGenerator {
+            num_groups: args.num_groups,
+            group_size: args.group_size,
+            variations_per_group: args.variations_per_group,
+        }
+    }
+}
+
+impl TokenGenerator for HierarchicalTokenGenerator {
+    fn next(&self, rng: &mut ThreadRng) -> Result<Vec<u32>> {
+        let mut tokens = Vec::new();
+        for _ in 0..self.num_groups {
+            let variant = rng.random_range(0..self.variations_per_group as u32);
+            for _ in 0..self.group_size {
+                tokens.push(variant);
+            }
+        }
+        Ok(tokens)
+    }
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Random(RandomTokenGeneratorArgs),
+    Hierarchical(HierarchicalTokenGeneratorArgs),
+}
+
+fn build_manager(args: &Args) -> Result<KvBlockManager<BasicMetadata>> {
+    let runtime_config = KvManagerRuntimeConfig::builder().worker_id(0).build()?;
+
+    let model_config = KvManagerModelConfig::builder()
+        .num_layers(args.num_layers)
+        .page_size(args.block_size)
+        .inner_dim(args.inner_dim)
+        .build()?;
+
+    let device_layout = KvManagerLayoutConfig::builder()
+        .num_blocks(args.num_device_blocks)
+        .allocator(DeviceAllocator::default())
+        .build()?;
+
+    let host_layout = if args.num_host_blocks > 0 {
+        Some(
+            KvManagerLayoutConfig::builder()
+                .num_blocks(args.num_host_blocks)
+                .allocator(PinnedAllocator::default())
+                .build()?,
+        )
+    } else {
+        None
+    };
+
+    let mut config_build = KvBlockManagerConfig::builder()
+        .runtime(runtime_config)
+        .model(model_config)
+        .device_layout(device_layout);
+
+    if let Some(host_layout) = host_layout {
+        config_build = config_build.host_layout(host_layout);
+    }
+
+    let config = config_build.build()?;
+
+    KvBlockManager::<BasicMetadata>::new(config)
+}
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let manager = build_manager(&args)?;
+
+    let token_generator: Arc<dyn TokenGenerator> = match &args.command {
+        Commands::Random(token_gen) => Arc::new(RandomTokenGenerator::from(token_gen.clone())),
+        Commands::Hierarchical(token_gen) => {
+            Arc::new(HierarchicalTokenGenerator::from(token_gen.clone()))
+        }
+    };
+
+    benchmark(manager, token_generator, args).await?;
+
+    Ok(())
+}
+
+async fn time<T, E: std::fmt::Debug>(f: impl Future<Output = Result<T, E>>) -> (T, Duration) {
+    let start = Instant::now();
+    let result = f.await.unwrap();
+    let duration = start.elapsed();
+    (result, duration)
+}
+
+struct EventStats {
+    time: Duration,
+    num_blocks: usize,
+}
+
+impl EventStats {
+    fn new(time: Duration, num_blocks: usize) -> Self {
+        Self { time, num_blocks }
+    }
+}
+
+struct Stats {
+    pub device_match_latency: Arc<Mutex<Vec<EventStats>>>,
+    pub host_match_latency: Arc<Mutex<Vec<EventStats>>>,
+    pub onboard_latency: Arc<Mutex<Vec<EventStats>>>,
+    pub device_allocate_latency: Arc<Mutex<Vec<EventStats>>>,
+    pub register_latency: Arc<Mutex<Vec<EventStats>>>,
+}
+
+impl Stats {
+    fn new() -> Self {
+        Self {
+            device_match_latency: Arc::new(Mutex::new(Vec::new())),
+            host_match_latency: Arc::new(Mutex::new(Vec::new())),
+            onboard_latency: Arc::new(Mutex::new(Vec::new())),
+            device_allocate_latency: Arc::new(Mutex::new(Vec::new())),
+            register_latency: Arc::new(Mutex::new(Vec::new())),
+        }
+    }
+}
+
+fn make_range(seq: Vec<f64>) -> Range<f64> {
+    let min = seq
+        .iter()
+        .map(|x| NotNan::new(*x).unwrap())
+        .min()
+        .unwrap()
+        .into_inner();
+    let max = seq
+        .iter()
+        .map(|x| NotNan::new(*x).unwrap())
+        .max()
+        .unwrap()
+        .into_inner();
+
+    (min - (max - min) * 0.1)..(max + (max - min) * 0.1)
+}
+
+// Taken from https://github.com/plotters-rs/plotters/blob/master/plotters/examples/normal-dist.rs
+async fn scatter(
+    name: &str,
+    file_name: &str,
+    latencies: Arc<Mutex<Vec<EventStats>>>,
+) -> Result<()> {
+    let drawing_area = BitMapBackend::new(file_name, (1920, 1080)).into_drawing_area();
+    drawing_area.fill(&WHITE)?;
+
+    let data = latencies
+        .lock()
+        .await
+        .iter()
+        .map(|e| (e.num_blocks as f64, e.time.as_micros() as f64))
+        .collect::<Vec<_>>();
+
+    let x = data.iter().map(|(x, _)| *x).collect::<Vec<_>>();
+    let y = data.iter().map(|(_, y)| *y).collect::<Vec<_>>();
+
+    // Create a chart builder
+    let mut chart = ChartBuilder::on(&drawing_area)
+        .caption(name, ("sans-serif", 50))
+        .margin(10)
+        .x_label_area_size(75)
+        .y_label_area_size(75)
+        .build_cartesian_2d(make_range(x), make_range(y))?;
+
+    // Configure the chart
+    chart
+        .configure_mesh()
+        .x_desc("Amount of blocks".to_string())
+        .y_desc("Latency (us)".to_string())
+        .draw()?;
+
+    // Draw the scatter plot
+    chart.draw_series(data.iter().map(|point| {
+        Circle::new(
+            *point,
+            4,
+            ShapeStyle {
+                color: RGBAColor(0, 0, 0, 0.25),
+                filled: true,
+                stroke_width: 1,
+            },
+        )
+    }))?;
+
+    // Save the result
+    drawing_area.present()?;
+
+    Ok(())
+}
+
+async fn benchmark(
+    manager: KvBlockManager<BasicMetadata>,
+    token_generator: Arc<dyn TokenGenerator>,
+    args: Args,
+) -> Result<()> {
+    let (req_tx, mut req_rx) = mpsc::unbounded_channel();
+    let manager = Arc::new(manager);
+
+    let mut rng = rand::rng();
+
+    println!("Generating inputs...");
+    let block_size = args.block_size;
+    let inputs: Vec<(Tokens, Vec<SequenceHash>)> = (0..args.requests_per_second * args.duration_s)
+        .progress()
+        .map(|_| {
+            let input_tokens = token_generator.next(&mut rng).unwrap();
+            let tokens = Tokens::from(input_tokens);
+
+            let sequence = tokens.clone().into_sequence(block_size, Some(0));
+            let sequence_hashes: Vec<SequenceHash> = sequence
+                .blocks()
+                .iter()
+                .map(|block| block.sequence_hash())
+                .collect();
+
+            (tokens, sequence_hashes)
+        })
+        .collect();
+
+    // Enqueue worker.
+    println!("Beginning benchmark...");
+    let enqueue_worker = tokio::spawn(async move {
+        let mut interval = interval(Duration::from_micros(
+            1000000 / args.requests_per_second as u64,
+        ));
+
+        for sequence_hashes in inputs.into_iter().progress() {
+            req_tx.send(sequence_hashes).unwrap();
+
+            tokio::select! {
+                _ = interval.tick() => {}
+                _ = tokio::signal::ctrl_c() => {
+                    break;
+                },
+            }
+        }
+    });
+
+    // Block manager worker.
+    let stats = Arc::new(Stats::new());
+    let stats_clone = stats.clone();
+    let block_manager_worker = tokio::spawn(async move {
+        while let Some((tokens, sequence_hashes)) = req_rx.recv().await {
+            let manager = manager.clone();
+            let stats = stats_clone.clone();
+            tokio::spawn(async move {
+                let device = manager.device().unwrap();
+                let host = manager.host().unwrap();
+
+                let mut sequence_blocks = Vec::new();
+
+                let (device_blocks, device_match_latency) =
+                    time(device.match_sequence_hashes(sequence_hashes.as_slice())).await;
+                stats
+                    .device_match_latency
+                    .lock()
+                    .await
+                    .push(EventStats::new(device_match_latency, device_blocks.len()));
+
+                sequence_blocks.extend(device_blocks);
+
+                if sequence_blocks.len() < sequence_hashes.len() {
+                    let (host_blocks, host_match_latency) =
+                        time(host.match_sequence_hashes(&sequence_hashes[sequence_blocks.len()..]))
+                            .await;
+
+                    stats
+                        .host_match_latency
+                        .lock()
+                        .await
+                        .push(EventStats::new(host_match_latency, host_blocks.len()));
+
+                    if !host_blocks.is_empty() {
+                        let (onboard_blocks, onboard_latency) =
+                            time(manager.onboard_blocks(host_blocks)).await;
+
+                        stats
+                            .onboard_latency
+                            .lock()
+                            .await
+                            .push(EventStats::new(onboard_latency, onboard_blocks.len()));
+
+                        sequence_blocks.extend(onboard_blocks);
+                    }
+                }
+
+                let remaining = sequence_hashes.len() - sequence_blocks.len();
+
+                if remaining > 0 {
+                    let (mut allocated_blocks, allocate_latency) =
+                        time(device.allocate_blocks(remaining)).await;
+
+                    stats
+                        .device_allocate_latency
+                        .lock()
+                        .await
+                        .push(EventStats::new(allocate_latency, allocated_blocks.len()));
+
+                    let mut prev_token_block = if let Some(last) = sequence_blocks.last() {
+                        if let BlockState::Registered(state) = last.state() {
+                            Some(state.token_block().clone())
+                        } else {
+                            panic!("Last block is not registered!");
+                        }
+                    } else {
+                        None
+                    };
+
+                    allocated_blocks
+                        .iter_mut()
+                        .enumerate()
+                        .for_each(|(i, block)| {
+                            let offset = (sequence_blocks.len() + i) * args.block_size;
+
+                            if offset + args.block_size > tokens.len() {
+                                return;
+                            }
+
+                            let mut partial_block = if let Some(prev) = prev_token_block.as_ref() {
+                                prev.next_block()
+                            } else {
+                                PartialTokenBlock::create_sequence_root(args.block_size, 0)
+                            };
+
+                            for token in &tokens[offset..offset + args.block_size] {
+                                partial_block.push_token(*token).unwrap();
+                            }
+
+                            let new_token_block = partial_block.commit().unwrap();
+
+                            prev_token_block = Some(new_token_block.clone());
+
+                            block.apply_token_block(new_token_block).unwrap();
+                        });
+
+                    let (registered_blocks, register_latency) =
+                        time(device.register_blocks(allocated_blocks)).await;
+
+                    stats
+                        .register_latency
+                        .lock()
+                        .await
+                        .push(EventStats::new(register_latency, registered_blocks.len()));
+
+                    sequence_blocks.extend(registered_blocks);
+                }
+            });
+        }
+    });
+
+    enqueue_worker.await?;
+    block_manager_worker.await?;
+
+    println!("Benchmark complete. Generating plots...");
+    scatter(
+        "Device Match Latency",
+        "device_match_latency.png",
+        stats.device_match_latency.clone(),
+    )
+    .await
+    .unwrap();
+    scatter(
+        "Host Match Latency",
+        "host_match_latency.png",
+        stats.host_match_latency.clone(),
+    )
+    .await
+    .unwrap();
+    scatter(
+        "Onboard Latency",
+        "onboard_latency.png",
+        stats.onboard_latency.clone(),
+    )
+    .await
+    .unwrap();
+    scatter(
+        "Device Allocate Latency",
+        "device_allocate_latency.png",
+        stats.device_allocate_latency.clone(),
+    )
+    .await
+    .unwrap();
+    scatter(
+        "Register Latency",
+        "register_latency.png",
+        stats.register_latency.clone(),
+    )
+    .await
+    .unwrap();
+    Ok(())
+}
