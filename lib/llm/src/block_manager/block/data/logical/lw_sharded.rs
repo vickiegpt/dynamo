f5b3ac09267c56c9c40c4e0bc0ad598935e8fe2b
Ryan Olson <rolson@nvidia.com>
2025-06-25 17:41:21 +0000

    add headers

--- lib/llm/src/block_manager/block/data/logical/lw_sharded.rs
+++ lib/llm/src/block_manager/block/data/logical/lw_sharded.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 use std::collections::HashMap;
 
 use crate::block_manager::block::nixl::{NixlBlockSet, RemoteBlocks, SerializedNixlBlockSet};
