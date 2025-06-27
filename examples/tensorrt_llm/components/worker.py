c049ac9ddebf50be8c8da86ea85be735bb15a305
tanmayv25 <tanmay2592@gmail.com>
2025-06-27 10:52:07 -0700

    Update disagg to run on single node

--- examples/tensorrt_llm/components/worker.py
+++ examples/tensorrt_llm/components/worker.py
@@ -37,7 +37,7 @@ logger = logging.getLogger(__name__)
     dynamo={
         "namespace": "dynamo",
     },
-    resources={"gpu": 1, "cpu": "10", "memory": "20Gi"},
+    resources={"cpu": "10", "memory": "20Gi"},
     workers=1,
 )
 class TensorRTLLMWorker(BaseTensorrtLLMEngine):
