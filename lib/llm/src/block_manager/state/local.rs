4327aa29934001ff248ad96c252a4469cf43bdac
jthomson04 <jwillthomson19@gmail.com>
2025-07-02 10:44:30 -0700

    Copyright, disable kvbm default feature

--- lib/llm/src/block_manager/state/local.rs
+++ lib/llm/src/block_manager/state/local.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 use super::*;
 
 /// The local block factories for the block manager
