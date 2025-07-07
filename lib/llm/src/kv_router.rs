0b106996c12845c675b785a8750d67b90f5a8d15
Biswa Panda <biswa.panda@gmail.com>
2025-07-06 22:26:46 -0700

    feat: allow targeting a backend instance

--- lib/llm/src/kv_router.rs
+++ lib/llm/src/kv_router.rs
@@ -243,8 +243,13 @@ impl AsyncEngine<SingleIn<PreprocessedRequest>, ManyOut<Annotated<LLMEngineOutpu
         match self.inner.client.instance_source.as_ref() {
             InstanceSource::Static => self.inner.r#static(request).await,
             InstanceSource::Dynamic(_) => {
-                let (instance_id, overlap_amount) =
-                    self.chooser.find_best_match(&request.token_ids).await?;
+                let (instance_id, overlap_amount) = if let Some(id) = request.backend_instance_id {
+                    // If instance_id is set, use it
+                    (id, 0)
+                } else {
+                    // Otherwise, find the best match
+                    self.chooser.find_best_match(&request.token_ids).await?
+                };
 
                 let query_instance_id = request.has_annotation("query_instance_id");
 
