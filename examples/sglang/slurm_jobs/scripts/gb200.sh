40c5d33015099c88f569d4b3b99b9fb777705b78
ishandhanani <ishandhanani@gmail.com>
2025-07-08 03:30:49 +0000

    cpy

--- examples/sglang/slurm_jobs/scripts/gb200.sh
+++ examples/sglang/slurm_jobs/scripts/gb200.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 # Function to print usage
 print_usage() {
