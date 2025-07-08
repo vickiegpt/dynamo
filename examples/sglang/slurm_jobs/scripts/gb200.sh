ba0cc3cc2b4487805b5a78d120d4b6d7cd779f0c
ishandhanani <ishandhanani@gmail.com>
2025-07-08 00:13:35 +0000

    Added env vars

--- examples/sglang/slurm_jobs/scripts/gb200.sh
+++ examples/sglang/slurm_jobs/scripts/gb200.sh
@@ -72,6 +72,12 @@ if [ "$mode" = "prefill" ]; then
         exit 1
     elif [ "$cmd" = "sglang" ]; then
         # GB200 sglang prefill command
+        SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=2048 \
+        NCCL_MNNVL_ENABLE=1 \
+        NCCL_CUMEM_ENABLE=1 \
+        SGLANG_USE_MESSAGE_QUEUE_BROADCASTER=0 \
+        SGL_DISABLE_TP_MEMORY_INBALANCE_CHECK=1 \
+        PYTHONUNBUFFERED=1 \
         python3 -m sglang.launch_server \
             --served-model-name deepseek-ai/DeepSeek-R1 \
             --model-path /model/ \
@@ -109,6 +115,8 @@ elif [ "$mode" = "decode" ]; then
         exit 1
     elif [ "$cmd" = "sglang" ]; then
         # GB200 sglang decode command
+        SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=768 \
+        SGLANG_NUM_RESERVED_DECODE_TOKENS=176 \
         python3 -m sglang.launch_server \
             --model-path /model/ \
             --trust-remote-code \
