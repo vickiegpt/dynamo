0257e74082b4cbcdcea27de2502509a28882c8f1
rihuo <rihuo@nvidia.com>
2025-07-10 09:35:19 -0700

    build from release

--- container/build_trtllm_wheel.sh
+++ container/build_trtllm_wheel.sh
@@ -44,12 +44,11 @@ MAIN_DIR=$(dirname "$(readlink -f "$0")")
 (cd /tmp && \
 # Clone the TensorRT-LLM repository.
 if [ ! -d "TensorRT-LLM" ]; then
-  git clone https://github.com/richardhuo-nv/TensorRT-LLM.git
+  git clone https://github.com/NVIDIA/TensorRT-LLM.git
 fi
 
 cd TensorRT-LLM
-
-git checkout rihuo/fix_broken_pipe
+git checkout release/0.21
 
 # Update the submodules.
 git submodule update --init --recursive
