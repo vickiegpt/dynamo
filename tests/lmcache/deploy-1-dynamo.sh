0fbf3e80a53064f2b6b5e2e0a41a8c1ae946649a
ZichengMa <zichengma1225@gmail.com>
2025-07-30 12:06:26 -0700

    fix copyright issue

--- tests/lmcache/deploy-1-dynamo.sh
+++ tests/lmcache/deploy-1-dynamo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 # ASSUMPTION: dynamo and its dependencies are properly installed
 # i.e. nats and etcd are running
