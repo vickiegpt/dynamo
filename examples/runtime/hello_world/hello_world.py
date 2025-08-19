5071bd83b5248e303777a44d04b266f213b258dd
michaelfeil <me@michaelfeil.eu>
2025-08-19 00:52:38 +0000

    rename to pycontext

--- examples/runtime/hello_world/hello_world.py
+++ examples/runtime/hello_world/hello_world.py
@@ -18,7 +18,7 @@ import logging
 
 import uvloop
 
-from dynamo.runtime import Context, DistributedRuntime, dynamo_endpoint, dynamo_worker
+from dynamo.runtime import PyContext, DistributedRuntime, dynamo_endpoint, dynamo_worker
 from dynamo.runtime.logging import configure_dynamo_logging
 
 logger = logging.getLogger(__name__)
@@ -26,7 +26,7 @@ configure_dynamo_logging(service_name="backend")
 
 
 @dynamo_endpoint(str, str)
-async def content_generator(request: str, context: Context):
+async def content_generator(request: str, context: PyContext):
     logger.info(f"Received request: {request} with `id={context.id()}`")
     for word in request.split(","):
         await asyncio.sleep(1)
