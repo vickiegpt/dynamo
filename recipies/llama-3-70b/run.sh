143e9f87f2602b59fbb43b1965a5cdce84833d7b
Biswa Panda <biswa.panda@gmail.com>
2025-08-29 15:42:45 -0700

    feat: add deployment and benchmarking job for llama3, agg, vllm

---
+++ recipies/llama-3-70b/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NAMESPACE=test-bis
+CUR_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+# create the pvc for model cache and download the model
+kubectl apply -n $NAMESPACE -f $CUR_DIR/model/model-cache.yaml
+kubectl apply -n $NAMESPACE -f $CUR_DIR/model/model-download.yaml
+# Wait for the model download to complete
+echo "Waiting for the model download to complete..."
+kubectl wait --for=condition=Complete job/model-download-llama-3-70b -n $NAMESPACE --timeout=6000s
+
+# deploy the agg example
+kubectl apply -n $NAMESPACE -f $CUR_DIR/agg/llama3-70b-agg.yaml
+
+# launch the benchmark job
+kubectl apply -n $NAMESPACE -f $CUR_DIR/agg/benchmark-job.yaml
+kubectl wait --for=condition=Complete job/llama-benchmark-job -n $NAMESPACE --timeout=6000s
+
+# print logs from the benchmark job
+kubectl logs job/llama-benchmark-job  -n $NAMESPACE
