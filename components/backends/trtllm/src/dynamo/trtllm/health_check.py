61d71b7b01cd64e08d354fca9c6c035f30c41cc2
tzulingk@nvidia.com <tzulingk@nvidia.com>
2025-09-15 20:12:04 -0700

    Update topk value, has to be >= 0.
    Signed-off-by: tzulingk@nvidia.com <tzulingk@nvidia.com>

--- components/backends/trtllm/src/dynamo/trtllm/health_check.py
+++ components/backends/trtllm/src/dynamo/trtllm/health_check.py
@@ -36,7 +36,7 @@ class TrtllmHealthCheckPayload(HealthCheckPayload):
             "sampling_options": {
                 "temperature": 0.0,
                 "top_p": 1.0,
-                "top_k": -1,
+                "top_k": 1,
                 "beam_width": 1,
                 "repetition_penalty": 1.0,
                 "presence_penalty": 0.0,
