16a7220189cd62060039dcf1445869d2fd8c8c88
Ziqi Fan <ziqif@nvidia.com>
2025-09-24 15:49:53 -0700

    feat: enable PD disagg for dynamo + vllm + kvbm
    Signed-off-by: Ziqi Fan <ziqif@nvidia.com>

---
+++ components/backends/vllm/launch/agg_kvbm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+set -e
+trap 'echo Cleaning up...; kill 0' EXIT
+
+# run ingress
+python -m dynamo.frontend --http-port=8000 &
+
+# run worker with KVBM enabled
+DYN_KVBM_CPU_CACHE_GB=4 \
+  python -m dynamo.vllm --model Qwen/Qwen3-0.6B --connector kvbm
