56913f1404d58acd4800f43580e9f4bb63972ae9
Anant Sharma <anants@nvidia.com>
2025-10-01 14:58:13 -0400

    Update hatch_build.py
    Signed-off-by: Anant Sharma <anants@nvidia.com>

--- hatch_build.py
+++ hatch_build.py
@@ -22,7 +22,6 @@ def get_components():
     components = []
     for item in os.listdir(components_dir):
         item_path = os.path.join(components_dir, item)
-        print(item_path)
         if os.path.isdir(item_path) and not item.startswith("."):
             components.append(item_path)
 
